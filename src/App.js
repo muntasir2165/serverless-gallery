347ad7526ed1df77f1d2e0e33a5284c182a539dc
Muntasir Biojid <muntasir2165@hotmail.com>
2020-12-18 20:37:08 -0700

    v-21 creating boilerplate for serverless backend

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World!
-        </p>
+        </p>np
         <a
           className="App-link"
           href="https://reactjs.org"
