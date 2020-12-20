913b0ef342e0b3483f8bbc941f40b972b90965bb
Muntasir Biojid <muntasir2165@hotmail.com>
2020-12-19 22:11:26 -0700

    v-2630 preparing front-end boilerplate 2

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+/* locals imports (all our reducers) */
+import myReducer from 'reducers/reducerTemplate';
+
+/* export reducers from a single entry point */
+export { myReducer };
---
+++ src/store.js
@@ -0,0 +1,19 @@
+/* external imports */
+import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+/* local imports (all our reducers) */
+import * as reducers from 'reducers';
+
+/* Use ES6 object literal shorthand syntax to define the object shape.
+ combine all reducers into one big store */
+const rootReducer = combineReducers({
+  ...reducers,
+});
+/* composer enhancers, if Redux Tools are present use it, otherise use
+the imported composer object. For more information about Redux Tools: http://extension.remotedev.io */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* create store and export */
+export default createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
