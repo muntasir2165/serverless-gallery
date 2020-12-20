3f1b1dda5f87e759ffc33c8a3d06d37284505dc3
Muntasir Biojid <muntasir2165@hotmail.com>
2020-12-19 20:16:19 -0700

    v-26 connecting frontend with the serverless backend 2

--- backend/scripts/config.js
+++ backend/scripts/config.js
@@ -5,9 +5,10 @@ function handler(data, serverless, options) {
   console.log('Received data', data);
   console.log('Received options', options);
 
-  let config = [`BUCKET_NAME=${data.examplebucketurl}`, `VALUE=${2}`].join(
-    '\n'
-  );
+  let config = [
+    `REACT_APP_BUCKET_NAME=${data.examplebucketurl}`,
+    `VALUE=${2}`,
+  ].join('\n');
 
   fs.writeFileSync(path.join(__dirname, `../../.${options.stage}.env`), config);
 }
